c9837169ccc9679c689c0f440cafb83cd438c5dd
Aguanine <aguanine@gmail.com>
2014-08-25 17:13:55 +0200

    update

--- Provisioning/urls.py
+++ Provisioning/urls.py
@@ -6,6 +6,8 @@ urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
     url(r'^index$', views.index, name='index'),
     # config/SN0123456789/MAC0123456789ABCDEF/PNSPA503/HW2.6.3/SW2.2.1
+    # config/$SN/$MAU/$PN/$HWVER/$SWVER
     url(r'^config/(?P<sn>[\w|\W]+)/(?P<mac>[\w|\W]+)/(?P<pdn>[\w|\W]+)/(?P<hwv>[\w|\W]+)/(?P<swv>[\w|\W]+)$', views.config),
+    # firmware/$SN/$MAU/$PN/$HWVER/$SWVER
     url(r'^firmware/(?P<sn>[\w|\W]+)/(?P<mac>[\w|\W]+)/(?P<pdn>[\w|\W]+)/(?P<hwv>[\w|\W]+)/(?P<swv>[\w|\W]+)$', views.firmware),
 )
