e58c12cc1981c57a01add143f8362b9b66d6410d
Aguanine <aguanine@gmail.com>
2016-03-30 16:54:41 +0200

    update

--- Provisioning/views.py
+++ Provisioning/views.py
@@ -68,7 +68,7 @@ def configok(request, sn, mac, pdn, hwv, swv):
     client = CurrentClientProduct.get_client()
     type_of_product = CurrentClientProduct.get_type_of_product()
     p = Product.get_product(sn, mac)
-    p.provok(True)
+    p.provok = True
     p.save()
     return StreamingHttpResponse("", content_type="text/xml")
 
